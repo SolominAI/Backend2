1426823465557272660bff0a744c3c2c2566882e
SolominAI <solandig@gmail.com>
2025-02-25 17:13:21 +1000

    Знакомство с FastApi

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
+
+
 @app.get('/')
 def func():
-    return 'Hello World'
+    return 'Hello World!'
 
 
-
-# E:\YandexDisk\4 0 Projects\lua_dashbord\import_export\export_stream\1726034697.1.1.txt
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
