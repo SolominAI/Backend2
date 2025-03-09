07f275b031a753f61ed864be7dd8c2e69ed10697
SolominAI <solandig@gmail.com>
2025-03-09 23:25:57 +1000

    Подключение Alembic

---
+++ src/migrations/versions/11440ded9057_initial_migration.py
@@ -0,0 +1,31 @@
+"""initial migration
+
+Revision ID: 11440ded9057
+Revises: 
+Create Date: 2025-03-09 22:19:27.885201
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '11440ded9057'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('hotels',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(length=100), nullable=False),
+    sa.Column('location', sa.String(), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('hotels')
