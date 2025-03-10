c3002ad7bc5f84c429ae05e9bad0cd09397c2c98
SolominAI <solandig@gmail.com>
2025-03-10 23:41:35 +1000

    добавил выражение на добавление отеля

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,10 @@
 from fastapi import Query, APIRouter, Body
 
+from sqlalchemy import insert
+
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
+from src.models.hotels import HotelsOrm
 from src.schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix='/hotels', tags=['Отели'])
@@ -37,22 +41,21 @@ def get_hotels(
     return hotels_
 
 @router.post('')
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     '1': {'summary': 'Сочи', 'value': {
-        'title': 'Отель Сочи 5 звезд у моря',
-        'name': 'sochi_u_morya'
+        'title': 'У моря',
+        'location': 'г.Сочи, ул. Морская 2'
     }},
     '2': {'summary': 'Дубай', 'value': {
         'title': 'У фантана',
-        'name': 'dubai_fountain'
+        'location': 'г.Дубай, ул. Шейха 1'
     }}
 })):
-    global hotels
-    hotels.append({
-        'id': int(hotels[-1]['id']) + 1,
-        'title': hotel_data.title,
-        'name': hotel_data.name
-    })
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
+
     return {'status': 'OK'}
 
 
