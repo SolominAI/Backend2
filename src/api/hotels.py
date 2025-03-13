87ed868e0ae023a3c514ea8c90fc7ef0099f45e4
SolominAI <solandig@gmail.com>
2025-03-14 00:05:57 +1000

    Задание 6: Обновление и удаление данных через репозиторий

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,8 +1,8 @@
-from fastapi import Query, APIRouter, Body
+from fastapi import Query, APIRouter, Body, HTTPException
+from sqlalchemy.exc import MultipleResultsFound
 
 from src.api.dependencies import PaginationDep
-from src.database import async_session_maker, engine
-from src.models.hotels import HotelsOrm
+from src.database import async_session_maker
 from src.schemas.hotels import Hotel, HotelPATCH
 
 from src.repositories.hotels import HotelsRepositories
@@ -45,12 +45,41 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     return {"status": "OK", "data": hotel}
 
 
-@router.put('/{hotel_id}')
-def edit_hotel(hotel_id: int, hotel_data: Hotel):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel['id'] == hotel_id][0]
-    hotel['title'] = hotel_data.title
-    hotel['name'] = hotel_data.name
+@router.put('')
+async def edit_hotel(
+    hotel_id: int | None = Query(None),
+    title: str | None = Query(None),
+    location: str | None = Query(None),
+    hotel_data: Hotel = Body(
+        openapi_examples={
+            '1': {
+                'summary': 'Сочи',
+                'value': {
+                    'title': 'У моря',
+                    'location': 'г.Сочи, ул. Морская 222'
+                }}
+        })):
+
+    filter_params = {}
+    if hotel_id:
+        filter_params['id'] = hotel_id
+    if title:
+        filter_params['title'] = title
+    if location:
+        filter_params['location'] = location
+
+    async with async_session_maker() as session:
+        try:
+            hotel = await HotelsRepositories(session).get_one_or_none(**filter_params)
+        except MultipleResultsFound:
+            raise HTTPException(status_code=400, detail='Найдено более одного отеля')
+
+        if hotel is None:
+            raise HTTPException(status_code=404, detail='Отель не найден')
+
+        await HotelsRepositories(session).edit(hotel_data, **filter_params)
+        await session.commit()
+
     return {'status': 'OK'}
 
 
@@ -69,8 +98,30 @@ def patch_hotel(hotel_id: int, hotel_data: HotelPATCH):
     return {'status': 'OK'}
 
 
-@router.delete('/{hotel_id}')
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel['id'] != hotel_id]
+@router.delete('')
+async def delete_hotel(
+    hotel_id: int | None = Query(None),
+    title: str | None = Query(None),
+    location: str | None = Query(None)
+):
+
+    filter_params = {}
+    if hotel_id:
+        filter_params['id'] = hotel_id
+    if title:
+        filter_params['title'] = title
+    if location:
+        filter_params['location'] = location
+
+    async with async_session_maker() as session:
+        try:
+            hotel = await HotelsRepositories(session).get_one_or_none(**filter_params)
+        except MultipleResultsFound:
+            raise HTTPException(status_code=400, detail='Найдено более одного отеля')
+
+        if hotel is None:
+            raise HTTPException(status_code=404, detail='Отель не найден')
+
+        await HotelsRepositories(session).delete(**filter_params)
+        await session.commit()
     return {'status': 'OK'}
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, update, delete
 from pydantic import BaseModel
 
 class BaseRepository:
@@ -21,3 +21,11 @@ class BaseRepository:
         add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         result = await self.session.execute(add_data_stmt)
         return result.scalars().one()
+
+    async def edit(self, data: BaseModel, **filter_by) -> None:
+        query = update(self.model).values(**data.model_dump()).filter_by(**filter_by)
+        await self.session.execute(query)
+
+    async def delete(self, **filter_by) -> None:
+        query = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(query)
