61daa7112a9ca6fe89363adb291f506b0a8d0ab4
SolominAI <solandig@gmail.com>
2025-04-30 11:07:46 +1000

    Задание 8. Уникальная почта для юзера

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+
+from passlib.context import CryptContext
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -6,15 +8,32 @@ from src.schemas.users import UserRequestAdd, UserAdd
 
 router = APIRouter(prefix='/auth', tags=['Авторизация и аутентификация'])
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 @router.post('/register')
 async def register_user(
-        data: UserRequestAdd,
+        data: UserRequestAdd = Body(openapi_examples={
+            'user1': {
+                'summary': 'Пример 1',
+                'value': {
+                    'email': 'test1@example.ru',
+                    'password': 'password123'
+                }
+            },
+            'user2': {
+                'summary': 'Пример 2',
+                'value': {
+                    'email': 'test2@example.ru',
+                    'password': 'password456'
+                }
+            }
+        })
 ):
-    hashed_password = '1231245'
+    hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        hotel = await UsersRepository(session).add(new_user_data)
+        await UsersRepository(session).add(new_user_data)
         await session.commit()
 
     return {"status": "OK"}
