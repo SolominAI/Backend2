5ac16fa11d633ec933fb5777d7e62167c50c3f4a
SolominAI <solandig@gmail.com>
2025-05-02 16:13:26 +1000

    задание 10 logout

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Body, HTTPException, Response, Request
+from fastapi import APIRouter, Body, HTTPException, Response
 
+from src.api.dependencies import UserIdDep
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
@@ -16,6 +17,7 @@ async def register_user(
                 'value': {
                     'email': 'test1@example.ru',
                     'password': 'password123'
+
                 }
             },
             'user2': {
@@ -65,13 +67,18 @@ async def login_user(
         return {"access_token": access_token}
 
 
-@router.get('/only_auth')
-async def only_auth(
-        request: Request,
+@router.get('/me')
+async def get_me(
+        user_id: UserIdDep,
 ):
-    access_token = request.cookies.get("access_token") or None
-    data = AuthService().decode_token(access_token)
-    user_id = data["user_id"]
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_one_or_none(id=user_id)
     return user
+
+
+@router.post('/logout')
+async def logout_user(
+    response: Response,
+):
+    response.delete_cookie("access_token")
+    return {"status": "OK"}
