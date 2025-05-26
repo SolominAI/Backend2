b89943ca3694e06b72f665555b0b497a2fe4a03d
SolominAI <solandig@gmail.com>
2025-05-26 19:17:56 +1000

    fixed16

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -79,12 +79,14 @@ async def edit_room(
                 "title": "Люкс",
                 "description": "Полностью заменённый номер с джакузи",
                 "price": 18000,
-                "quantity": 3
+                "quantity": 3,
+                "facilities_ids": [1, 2]
             }}
         })
 ):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     await db.rooms.edit(_room_data, id=room_id)
+    await db.rooms_facilities.set_room_facilities(room_id, facilities_ids=room_data.facilities_ids)
     await db.commit()
     return {'status': 'OK'}
 
@@ -100,8 +102,11 @@ async def patch_room(
         room_id: int = Path(..., description='ID номера'),
         room_data: RoomPatchRequest = Body(...)
 ):
+    _room_data_dict = room_data.model_dump(exclude_unset=True)
     _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
     await db.rooms.edit(_room_data, exclude_unset=True, id=room_id, hotel_id=hotel_id)
+    if 'facilities_ids' in _room_data_dict:
+        await db.rooms_facilities.set_room_facilities(room_id, facilities_ids=_room_data_dict["facilities_ids"])
     await db.commit()
     return {'status': 'OK'}
 
