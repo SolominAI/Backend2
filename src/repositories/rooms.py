308d8fa69eb725c143d9c072b7a031859fc8e61e
SolominAI <solandig@gmail.com>
2025-05-26 20:21:03 +1000

    task17 fixed

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
 
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
@@ -30,17 +30,14 @@ class RoomsRepository(BaseRepository):
         result = await self.session.execute(query)
         return [RoomWithRels.model_validate(model) for model in result.unique().scalars().all()]
 
-    async def get_one_with_facilities(
-            self,
-            room_id: int,
-            hotel_id: int
-    ):
+    async def get_one_or_none_with_rels(self, **filter_by):
         query = (
             select(self.model)
-            .options(joinedload(self.model.facilities))
-            .filter(self.model.id == room_id, self.model.hotel_id == hotel_id)
+            .options(selectinload(self.model.facilities))
+            .filter_by(**filter_by)
         )
-
         result = await self.session.execute(query)
-        room = result.scalars().unique().one_or_none()
-        return RoomWithRels.model_validate(room) if room else None
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        return RoomWithRels.model_validate(model)
