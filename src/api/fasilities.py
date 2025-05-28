9cea09ad3c87cfa9a820e0742be3dde874404726
SolominAI <solandig@gmail.com>
2025-05-28 20:48:08 +1000

    add lifespan, use RedisManager to cache /facilities

--- src/api/fasilities.py
+++ src/api/fasilities.py
@@ -1,4 +1,7 @@
+import json
+
 from fastapi import APIRouter, Body
+from src.init import redis_manager
 from src.schemas.fasilities import FacilityAdd
 from src.api.dependencies import DBDep
 
@@ -7,7 +10,19 @@ router = APIRouter(prefix='/facilities', tags=['Удобства'])
 
 @router.get('')
 async def get_facilities(db: DBDep):
-    return await db.facilities.get_all()
+    facilities_from_cache = await redis_manager.get("facilities")
+    print(f"{facilities_from_cache=}")
+    if not facilities_from_cache:
+        print(f"Иду в базу данных")
+        facilities = await db.facilities.get_all()
+        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
+        facilities_json = json.dumps(facilities_schemas)
+        await redis_manager.set('facilities', facilities_json, 10)
+
+        return facilities
+    else:
+        facilities_dicts = json.loads(facilities_from_cache)
+        return facilities_dicts
 
 
 @router.post('')
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 
 import sys
 from pathlib import Path
+
+from src.init import redis_manager
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -13,7 +17,14 @@ from src.api.bookings import router as router_bookings
 from src.api.fasilities import router as router_fasilities
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_manager.connect()
+    yield
+    await redis_manager.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router_auth)
 app.include_router(router_rooms)
