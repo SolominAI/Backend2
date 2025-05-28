052b76aa74fca3e825066a4311b9e2a2ce63f874
SolominAI <solandig@gmail.com>
2025-05-28 19:52:08 +1000

    add mappers

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,8 +1,9 @@
 from src.models.bookings import BookingsOrm
-from src.schemas.bookings import Booking
+from src.repositories.mappers.mappers import BookingDataMapper
+
 from src.repositories.base import BaseRepository
 
 
 class BookingRepository(BaseRepository):
     model = BookingsOrm
-    schema = Booking
+    mapper = BookingDataMapper
