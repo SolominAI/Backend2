88b7714d8aad840757c775b73e0f49ddc6a7118b
SolominAI <solandig@gmail.com>
2025-06-04 16:11:37 +1000

    task19 fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,12 @@ import pytest
 from httpx import AsyncClient
 
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.main import app
 from src.models import *
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomAdd
+from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -20,22 +23,18 @@ async def setup_database(check_test_mode):
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
+    with open("tests/mock_hotels.json", encoding="utf-8") as file_hotels:
+        hotels = json.load(file_hotels)
+    with open("tests/mock_rooms.json", encoding="utf-8") as file_rooms:
+        rooms = json.load(file_rooms)
 
-@pytest.fixture(scope="session", autouse=True)
-async def insert_mock_hotels_and_rooms(setup_database):
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        with open("tests/mock_hotels.json", encoding="utf-8") as f:
-            hotels_data = json.load(f)
+    hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
+    rooms = [RoomAdd.model_validate(room) for room in rooms]
 
-        for hotel in hotels_data:
-            await ac.post("/hotels", json=hotel)
-
-        with open("tests/mock_rooms.json", encoding="utf-8") as f:
-            rooms_data = json.load(f)
-
-        for room in rooms_data:
-            hotel_id = room.pop("hotel_id")
-            await ac.post(f"/hotel/{hotel_id}/rooms", json=room)
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels)
+        await db.rooms.add_bulk(rooms)
+        await db.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
