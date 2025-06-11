d280d5a12c3fec85cc862ef50a0ff5d6cb609992
SolominAI <solandig@gmail.com>
2025-06-11 14:44:55 +1000

    create add_booking() to check whether room can be booked

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -33,6 +33,7 @@ async def create_booking(
         }),
 ):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    hotel = await db.hotels.get_one_or_none(id=room.hotel_id)
     if room is None:
         raise HTTPException(status_code=404, detail='Комната не найдена')
 
@@ -42,7 +43,7 @@ async def create_booking(
         price=room_price,
         **booking_data.model_dump()
     )
-    booking = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
     await db.commit()
     return {"status": "OK", "data": booking}
 
